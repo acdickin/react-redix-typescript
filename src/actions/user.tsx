90276035d2f3444c337cb11aa237bbaf80b8e74b
Andrew <acdickin@gmail.com>
2018-09-24 15:46:19 -0700

    Fix redux naming issue

--- src/actions/user.tsx
+++ src/actions/user.tsx
@@ -1,8 +1,8 @@
 import IUser from '../models/User'
 
-// export const CREATE_USER = 'CREATE_USER'
-// export const UPDATE_USER = 'UPDATE_USER'
-// export const DELETE_USER = 'DELETE_USER'
+export const CREATE_USER = 'CREATE_USER'
+export const UPDATE_USER = 'UPDATE_USER'
+export const DELETE_USER = 'DELETE_USER'
 
 export enum ActionTypes{
   CREATE_USER = 'CREATE_USER',
